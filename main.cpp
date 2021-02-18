53eae7658a58d99b371a7a5b3c2618a5039905e9
nhatvu148 <nhatvu148@gmail.com>
2021-02-19 02:27:07 +0900

    add lib with levels of CMake

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
+#include <adder.h>
 
-int main(int, char**) {
+int main(int, char **)
+{
     std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
+
+    std::cout << add(1, 2) << "\n";
+    return 0;
 }
