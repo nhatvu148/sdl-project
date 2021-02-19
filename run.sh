2eb1ce21163b96321418e76a171d9d54606a28fc
Akiyama <nhatvu148@gmail.com>
2021-02-19 18:59:23 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,15 @@
 cd build
 
 FILE="./sdlproject"
+DEBUG="./Debug/sdlproject"
+RELEASE="./Release/sdlproject"
 if [[ -s $FILE ]]; then
     echo "$FILE exists and not empty"
     ./sdlproject
-else
-    echo "$FILE doesn't exist or is empty"
+elif  [[ -s $DEBUG ]]; then
+    echo "Running $DEBUG"
     ./Debug/sdlproject
+else
+    echo "Running $RELEASE"
+    ./Release/sdlproject
 fi
